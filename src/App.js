1bb26ac4c8d94670a787367b167377249249be15
Anton Meshchaninov <tonym7@yandex.ru>
2022-06-25 18:53:39 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+
+			<header className="header">
+
+				<div className="header__left">
+					<img src="/img/logo.png" alt="Logo" />
+					<div className="header__info">
+						<h3>React sneakers</h3>
+						<p>Магазин лучших кроссовок</p>
+					</div>
+				</div>
+
+				<ul className="header__rigth">
+					<li>
+						<img src="/img/cart.svg" alt="Logo" />
+						<span>1205 руб.</span>
+					</li>
+					<li>
+						<img src="/img/user.svg" alt="Logo" />
+					</li>
+				</ul>
+
+			</header>
+
+			<div className="content">
+
+				<h1>Все кроссовки</h1>
+
+				...
+
+			</div>
+
+		</div>
+	)
 }
 
 export default App;
